e5b65d851e7618ac1dee3711ef157841285e9599
Roberto Cruz <robcruz088@gmail.com>
2022-06-17 19:52:44 -0400

    Flask implementation
    + simple flask implementation
    + duck

--- Web App/main_app.py
+++ Web App/main_app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, make_response
-from time import time
 
 app = Flask(__name__)
 
-@app.route()
+@app.route("/", methods=['GET', 'POST'])
 def main():
-    return
+    return render_template("base.html")
 
-@app.route('/data', methods=["GET","POST"])
-def data():
-    return
+@app.route("/charts", methods=['GET', 'POST'])
+def charts():
+    return render_template("charts.html")
+
+@app.route("/info", methods=['GET', 'POST'])
+def info():
+    return render_template("info.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-
+#               _
+#  quack      >(.)__
+#              (___/
